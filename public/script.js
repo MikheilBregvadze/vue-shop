3ac2d406f14a742b1e6f27f6f383b2c15ff7bd29
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2019-01-13 00:31:13 -0800

    merge

---
+++ public/script.js
@@ -0,0 +1,64 @@
+var PRICE = 9.99;
+new Vue({
+	el: '#app',
+
+	data: {
+		total: 0,
+		items: [],
+		cart: [],
+		newSearch: '',
+		lastSearch: ''
+	},
+	methods: {
+		onSubmit() {
+			this.$http
+				.get('/search/'.concat(this.newSearch))
+				.then(function(res) {
+					this.lastSearch = this.newSearch;
+					this.items = res.data;
+				})
+			;
+		},
+		addItem(index) {
+			this.total += PRICE;
+			var item = this.items[index]; 
+			var found = false;
+			for(var i = 0; i < this.cart.length; i++) {
+				if(this.cart[i].id === item.id) {
+					found = true;
+					this.cart[i].qty++;
+					break;
+				}
+			}
+			if(!found) {
+				this.cart.push({
+					id: item.id,
+					title: item.title,
+					qty: 1,
+					price: PRICE
+				});
+			}
+		},
+		inc(item) {
+			item.qty++;
+			this.total +=PRICE;
+		},
+		dec(item) {
+			item.qty--;
+			this.total -=PRICE;
+			if(item.qty <= 0) {
+				for(var i = 0; i < this.cart.length; i++) {
+					if(this.cart[i].id === item.id) {
+						this.cart.splice(i, 1);
+						break;
+					}
+				}
+			}
+		}
+	},
+	filters: {
+		currency(price) {
+			return '$'.concat(price.toFixed(2));
+		}
+	}
+});
